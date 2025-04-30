28cf7a89bf77e418c5145f29a55d952bbd075300
samuel <samuelgarcia201586@gmail.com>
2025-04-30 13:35:59 -0500

    definir metodos de la clase reglavalidacion

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -1,14 +1,30 @@
+from abc import ABC, abstractmethod
+import re
+from validadorclave.modelo.errores import (
+    NoCumpleLongitudMinimaError,
+    NoTieneLetraMayusculaError,
+    NoTieneLetraMinusculaError,
+    NoTieneNumeroError,
+    NoTieneCaracterEspecialError,
+    NoTienePalabraSecretaError
+)
 
+class ReglaValidacion(ABC):
+    def __init__(self, longitud_esperada):
+        self._longitud_esperada = longitud_esperada
 
+    def _validar_longitud(self, clave):
+        return len(clave) > self._longitud_esperada
 
-class ReglaValidacion:
-    pass
+    def _contiene_mayuscula(self, clave):
+        return any(c.isupper() for c in clave)
 
-class ReglaValidacionGanimedes:
-    pass
+    def _contiene_minuscula(self, clave):
+        return any(c.islower() for c in clave)
 
-class ReglaValidacionCalisto:
-    pass
+    def _contiene_numero(self, clave):
+        return any(c.isdigit() for c in clave)
 
-class Validador:
-    pass
+    @abstractmethod
+    def es_valida(self, clave):
+        pass
