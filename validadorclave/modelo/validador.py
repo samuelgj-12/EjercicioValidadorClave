a807bf9ced5e45bf3a315df3b6f1153ea4999d01
samuel <samuelgarcia201586@gmail.com>
2025-04-30 13:38:48 -0500

    definir metodos de la clase reglavalidacioncalisto

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -49,3 +49,32 @@ class ReglaValidacionGanimedes(ReglaValidacion):
         if not self.contiene_caracter_especial(clave):
             raise NoTieneCaracterEspecialError()
         return True
+
+class ReglaValidacionCalisto(ReglaValidacion):
+    def __init__(self):
+        super().__init__(6)
+
+    def contiene_calisto(self, clave):
+        match = re.search(r"calisto", clave, re.IGNORECASE)
+        if match:
+            palabra = clave[match.start():match.end()]
+            mayus = sum(1 for c in palabra if c.isupper())
+            return 2 <= mayus < len(palabra)
+        return False
+
+    def es_valida(self, clave):
+        if not self._validar_longitud(clave):
+            raise NoCumpleLongitudMinimaError()
+        if not self._contiene_numero(clave):
+            raise NoTieneNumeroError()
+        if not self.contiene_calisto(clave):
+            raise NoTienePalabraSecretaError()
+        return True
+
+
+class Validador:
+    def __init__(self, regla):
+        self.regla = regla
+
+    def es_valida(self, clave):
+        return self.regla.es_valida(clave)
