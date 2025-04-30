3a2034df77ed2be1bd2938b231bb91b97a8210e4
samuel <samuelgarcia201586@gmail.com>
2025-04-30 13:37:06 -0500

    definir metodos de la clase reglavalidacionganimedes

--- validadorclave/modelo/validador.py
+++ validadorclave/modelo/validador.py
@@ -28,3 +28,24 @@ class ReglaValidacion(ABC):
     @abstractmethod
     def es_valida(self, clave):
         pass
+
+
+class ReglaValidacionGanimedes(ReglaValidacion):
+    def __init__(self):
+        super().__init__(8)
+
+    def contiene_caracter_especial(self, clave):
+        return any(c in "@_#$%" for c in clave)
+
+    def es_valida(self, clave):
+        if not self._validar_longitud(clave):
+            raise NoCumpleLongitudMinimaError()
+        if not self._contiene_mayuscula(clave):
+            raise NoTieneLetraMayusculaError()
+        if not self._contiene_minuscula(clave):
+            raise NoTieneLetraMinusculaError()
+        if not self._contiene_numero(clave):
+            raise NoTieneNumeroError()
+        if not self.contiene_caracter_especial(clave):
+            raise NoTieneCaracterEspecialError()
+        return True
